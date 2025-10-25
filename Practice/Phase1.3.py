13b8b04c906ad83fc4db80f369049969312b0786
arsh <arshdeepkhalsa60@gmail.com>
2025-10-25 19:08:08 +0530

    Added phase 1.3

--- Practice/Phase1.3.py
+++ Practice/Phase1.3.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+arr = np.arange(1, 101).reshape(10, 10)
+print(arr)
+
+for i in range(0, 10, 2):
+  print(arr[i])
+
+for i in range(0, 10, 1):
+  print(arr[i][i])
+
+subamt = arr[2:6, 4:9]
+print(subamt)
